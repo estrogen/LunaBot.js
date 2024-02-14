bfdbaf0b27212beb308da942affd14bd105f0351
Rynnth <rynnth@gmail.com>
2024-02-14 12:16:17 +0100

    IGN Commands

---
+++ src/commands/Staff/ign.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const users = require('../../models/dbv2/usersSchema');
+const cc = require('../../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ign')
+        .setDescription('get or update the ign of a clan member')
+        .addStringOption(option => option.setName('user').setDescription('Warframe Player').setRequired(true))
+        .addStringOption(option => option.setName('ign').setDescription('Updated IGN').setRequired(true))
+        .addStringOption(option => option.setName('nickname').setDescription('Update Users Nickname').setRequired(false)
+            .addChoices(
+                {name: 'True', value: '1'},
+                {name: 'False', value: '0'}
+                ))
+        .setDefaultPermission(false),
+
+    async execute(i, bot) {
+        if(!i.member.roles.cache.some(r => cc.Roles.Staff.includes(r.id))){
+            return i.reply({ content: "You're not a staff!", ephemeral: true});
+        }
+
+        const user = i.options.getString('user');
+        const ign = i.options.getString('ign');
+
+        const member = await i.guild.members.fetch(user);
+        if (!member){ 
+            return i.reply({ content: 'Unable to find member.', ephemeral: true });
+        }
+       
+        if(i.options.getString('nickname') == 1){
+            await member.setNickname(ign, `In game name updated by ${i.user.tag}`);
+        }
+        
+        let userData = await users.findOne({ userID: member.id });
+        if(!userData){
+            return i.reply({ content: "This user hasn't been recruited", ephemeral: true});
+        }
+        if(userData.wfIGN != ""){
+            userData.wfPastIGN.push(ign);
+        }
+        userData.wfIGN = ign;
+        await userData.save();
+        
+        await i.reply({ content: `User: <@${user}> had their ign updated to (${ign})`});
+    }
+}
--- src/models/dbv2/wf_recruitData.js
+++ src/models/dbv2/wf_recruitData.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const wf_recruitData = {
     userID: { type: String, required: true, index: true },
-    recruiter: { type: String, required: true, index: true },
-    joinDate: { type: Date, required: true, index: true },
-    kingdom: { type: String, required: true }
+    recruiter: { type: String, required: true, index: true},
+    joinDate: {type: Date, required: true, index: true},
+    clan: { type: String, required: true}
+
 };
 
 module.exports = mongoose.model("wf_recruitData", wf_recruitData, "wf_recruitData");
